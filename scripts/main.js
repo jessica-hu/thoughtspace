7e5934ee28277b0b271088f3c7eb6c446c7d463f
Jessica Hu <jhu5749@gmail.com>
2016-06-07 21:03:06 -0500

    init commit

---
+++ scripts/main.js
@@ -0,0 +1,18 @@
+$(function(){
+        $('img').imgPreload()
+    })
+
+
+// init Masonry
+var $grid = $('.grid').masonry({
+  // set itemSelector so .grid-sizer is not used in layout
+  itemSelector: '.grid-item',
+  // use element for option
+  columnWidth: '.grid-sizer',
+  percentPosition: true
+})
+
+// layout Masonry after each image loads
+$grid.imagesLoaded().progress( function() {
+  $grid.masonry('layout');
+});
